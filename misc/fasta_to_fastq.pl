9741ba827f2c9240155c83bfe181d80c5e0daef3
Scott G. Daniel <scottdaniel25@gmail.com>
2015-10-30 16:04:00 -0700

    fixing the fasta_to_fastq script

--- misc/fasta_to_fastq.pl
+++ misc/fasta_to_fastq.pl
@@ -1,3 +1,87 @@
+#!/rsgrps/bhurwitz/hurwitzlab/bin/perl
+
+use common::sense;
+use autodie;
+use Getopt::Long;
+use Pod::Usage;
+use Readonly;
+
+main();
+
+# --------------------------------------------------
+sub main {
+    my %args = get_args();
+
+    if ($args{'help'} || $args{'man'}) {
+        pod2usage({
+            -exitval => 0,
+            -verbose => $args{'man'} ? 2 : 1
+        });
+    }; 
+
+    pod2usage(1);
+}
+
+# --------------------------------------------------
+sub get_args {
+    my %args;
+    my $file;
+    my $outfile;
+    GetOptions(
+        \%args,
+        'f|file=s'      =>  \$file,
+        'o|outfile=s'   =>  \$outfile,
+        'help',
+        'man',
+    ) or pod2usage(2);
+
+    return %args;
+}
+
+__END__
+
+# --------------------------------------------------
+
+=pod
+
+=head1 NAME
+
+fastaToFastq.pl - a script to add fake quality scores to fasta and make it a fastq
+
+=head1 SYNOPSIS
+
+  fastaToFastq.pl 
+
+Options:
+
+  -f|--file     fasta file that will be converted
+  -o|--outfile  fastq file that will be written
+  --help        Show brief help and exit
+  --man         Show full documentation
+
+=head1 DESCRIPTION
+
+Wants a fasta file for input, spits out a fastq
+
+=head1 SEE ALSO
+
+perl.
+
+=head1 AUTHOR
+
+Scott Daniel E<lt>scottdaniel@email.arizona.eduE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2015 Hurwitz Lab
+
+This module is free software; you can redistribute it and/or
+modify it under the terms of the GPL (either version 1, or at
+your option, any later version) or the Artistic License 2.0.
+Refer to LICENSE for the full license text and to DISCLAIMER for
+additional warranty disclaimers.
+
+=cut
 #Copyright (c) 2010 LUQMAN HAKIM BIN ABDUL HADI (csilhah@nus.edu.sg)
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
